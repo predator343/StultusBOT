098a844596818af276835bd2c728d35b19ea6163
faultydev <didierslof@gmail.com>
2020-12-27 21:17:13 +0100

    help command, example is now updated.

--- commands/enabled/util_help.js
+++ commands/enabled/util_help.js
@@ -4,11 +4,14 @@ const config = require("../../config.json");
 
 module.exports.run = async (bot, message, args) => {
   //this is where the actual code for the command goes
-   
+   embed.setTitle("click me!");
+   embed.setURL("https://codeducks.github.io/xBOT");
+
+   message.channel.send(embed);
 }
 
 //name this whatever the command name is.
 module.exports.help = {
-  name: 'x',
-  aliases: []
+  name: 'help',
+  aliases: ['commands', 'helpme']
 }
