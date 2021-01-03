329b9af5a6a315f44af26327cda700c3ebffa0e5
faultydev <didierslof@gmail.com>
2021-01-03 01:07:36 +0100

    err fix

--- commands/example/example.js
+++ commands/example/example.js
@@ -5,10 +5,7 @@ const config = require("../../configs/global.json");
 module.exports.run = async (bot, message, args) => {
   //this is where the actual code for the command goes
   const embed = new Discord.MessageEmbed();
-<<<<<<< HEAD
-  message.channel.send("this is an example");
-=======
->>>>>>> master
+  message.channel.send("this is an example");ter
 }
 
 //name this whatever the command name is.
